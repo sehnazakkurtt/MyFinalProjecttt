359b88d388e2f7823d4c64eaca8594aa83d65b3f
sehnazakkurtt <60717621+sehnazakkurtt@users.noreply.github.com>
2022-06-20 00:51:04 +0300

    Add files via upload

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        //categori ile ilgili dış dünyaya neyi servis etmek istiyorsam
+
+        List<Category> GetAll(); //tümünü listele
+        Category GetById(int categoryId);
+
+    }
+}
