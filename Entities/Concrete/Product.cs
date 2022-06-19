fc800a71a38e9fb5f7ff1adbb7cb792230c77ab5
sehnazakkurtt <60717621+sehnazakkurtt@users.noreply.github.com>
2022-06-20 00:57:19 +0300

    Add files via upload

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity  //IEntity: Bu bir veritabanı nesnesidir demek
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short  UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
