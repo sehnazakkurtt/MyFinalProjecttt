7d79f2b0b438156882a48deabd41bb3b3efea5db
sehnazakkurtt <60717621+sehnazakkurtt@users.noreply.github.com>
2022-06-20 00:56:18 +0300

    Add files via upload

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System.Collections.Generic;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        //veri erişim işlerini yaoacaj
+
+        //ürüne ait özel işler hoin gibi
+
+        List<ProductDetailDto> GetProductDetailDtos();
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Context nesnesi Db tabloları ile proje classlarını bağlamak,ilişkilendirmek
+    public class NorthwindContext : DbContext  //contextimixin ta kendisi. DbContext entityframework ile beraber geliyor
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //bu method proje hangi veritabı ile ilişkili olduğunu belirteceğimiz yer
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database =Northwind;Trusted_Connection=true"); // sql serves adres tanımlama //context hangi veritabanına bağlanacağını buldu burda 
+        }
+
+        public DbSet<Product> Products { get; set; } // burda da diyor ki benim hangi clasım hangi tabloya karşılık geliyor => //benim produtc nesnemi veritabanındaki products ile bağla
+        public DbSet<Category> Categories { get; set; } //veritabanı ile tabloları ilişkilendirdiğimiz yer
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
